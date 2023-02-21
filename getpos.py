b3ce771dd1efaa3dfb260e09205a40e9d95e0d4f
haruto17 <50482005+haruto17@users.noreply.github.com>
2023-02-21 22:35:34 +0900

    台形補正テスト(getposは座標取得用)

---
+++ getpos.py
@@ -0,0 +1,18 @@
+import cv2 as cv
+import numpy as np
+
+def getpos():
+    img = cv.imread('a1.png')
+
+    def click_pos(event,x,y,flags,params):
+        if event == cv.EVENT_LBUTTONDOWN:
+            img2 = np.copy(img)
+            cv.circle(img2,center=(x,y),radius=5,color=255,thickness=-1)
+            pos_str='(x,y)=('+str(x)+','+str(y)+')'
+            cv.putText(img2,pos_str,(x+10, y+10),cv.FONT_HERSHEY_PLAIN,2,255,2,cv.LINE_AA)
+            cv.imshow('window', img2)
+            print(x,y)
+    cv.imshow('window',img)
+    cv.setMouseCallback('window',click_pos)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import takeshot
 import resize
 import cut
+import getpos
+import perstransform
 
 if __name__ == '__main__':
     #takeshot.takescreenshot()
     #resize.img_resize()
     cut.img_cut()
+    perstransform.perspective_transform()
