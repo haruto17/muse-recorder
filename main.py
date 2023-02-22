6d04776dd29b410fe1e8b3dbfdcd8486999f41c0
haruto17 <50482005+haruto17@users.noreply.github.com>
2023-02-22 19:20:24 +0900

    達成率部分の台形補正とOCRテスト

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     #takeshot.takescreenshot()
     #resize.img_resize()
     # cut.img_cut()
-    #perstransform.perspective_transform()
+    # perstransform.pers_transform()
     #getpos.getpos()
     ocr.img_ocr()
--- perstransform.py
+++ perstransform.py
@@ -2,15 +2,15 @@ import cv2 as cv
 import numpy as np
 import math
 
-def perspective_transform():
-    w_ratio = 1.2
+def perspective_transform(input_img_path,output_img_path,ratio,p1,p2,p3,p4):
+    w_ratio = ratio
 
-    p1 = np.array([150,30])
-    p2 = np.array([510,160])
-    p3 = np.array([120,80])
-    p4 = np.array([495,200])
+    img = cv.imread(input_img_path)
 
-    img = cv.imread('b1.png')
+    p1 = np.array(p1)
+    p2 = np.array(p2)
+    p3 = np.array(p3)
+    p4 = np.array(p4)
 
     o_width = np.linalg.norm(p2-p1)
     o_width = math.floor(o_width * w_ratio)
@@ -25,4 +25,12 @@ def perspective_transform():
 
     output = cv.warpPerspective(img,M,(o_width,o_height))
 
-    cv.imwrite('name.png',output)
+    cv.imwrite(output_img_path,output)
+
+def pers_transform():
+
+    # music name
+    perspective_transform('b1.png','name.png',1.2,[150,30],[510,160],[120,80],[495,200])
+
+    # percentage of achievement
+    perspective_transform('b1.png','achievement.png',1.2,[190,105],[300,140],[170,140],[285,175])
