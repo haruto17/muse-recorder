9ee14945c0338d94a3a453a0891ffa2a83d7096e
haruto17 <50482005+haruto17@users.noreply.github.com>
2023-02-22 13:23:15 +0900

    文字認識テスト（良好）

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import resize
 import cut
 import getpos
 import perstransform
+import ocr
 
 if __name__ == '__main__':
     #takeshot.takescreenshot()
     #resize.img_resize()
     # cut.img_cut()
-    perstransform.perspective_transform()
+    #perstransform.perspective_transform()
     #getpos.getpos()
+    ocr.img_ocr()
