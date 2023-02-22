4e637e4b140bd20e9b43330f96d2709165669a68
haruto17 <50482005+haruto17@users.noreply.github.com>
2023-02-22 12:00:44 +0900

    カット部分の範囲調整

--- cut.py
+++ cut.py
@@ -3,7 +3,7 @@ import cv2 as cv
 def img_cut():
     img = cv.imread('a.png')
 
-    img_b1 = img[420:700,50:550]
+    img_b1 = img[400:700,30:550]
     cv.imwrite('b1.png',img_b1)
 
     img_b2 = img[350:650,800:1230]
