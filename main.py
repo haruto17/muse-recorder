768bf8100a10da5d5afd4b11e95e969c5a8616f5
haruto17 <s1902272@sendai-nct.jp>
2023-03-04 00:42:56 +0900

    削除

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-import takeshot
-import resize
-import cut
-import getpos
-import perstransform
-import ocr
-import threshold
+import flet as ft
 
-if __name__ == '__main__':
-    # takeshot.takescreenshot()
-    # resize.img_resize()
-    # cut.img_cut()
-    perstransform.pers_transform()
-    threshold.img_threshold()
-    #getpos.getpos()
-    ocr.img_ocr()
+
+def main(page):
+    page.title = "Muse-Recorder"
+
+    input_name = ft.TextField(label="Music Name", autofocus=True)
+    input_achi = ft.TextField(label="Achievement")
+    input_combo = ft.TextField(label="Max Combo")
+    input_perfect = ft.TextField(label="Perfect")
+    input_great = ft.TextField(label="Great")
+    input_pass = ft.TextField(label="Pass")
+    input_miss = ft.TextField(label="Miss")
+
+    def btn_click(e):
+        page.update()
+
+    page.theme_mode = "dark"
+    page.window_width = 300
+    page.window_height = 700
+    page.add(
+        input_name,
+        input_achi,
+        input_combo,
+        input_perfect,
+        input_great,
+        input_pass,
+        input_miss,
+        ft.ElevatedButton("Add", on_click=btn_click),
+    )
+
+ft.app(target=main)
